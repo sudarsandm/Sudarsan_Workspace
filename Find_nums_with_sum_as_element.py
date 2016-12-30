4672eeed6716a531d00ae3f04452111e506623be
sudarsandm <sudarsandm@gmail.com>
2016-12-30 13:11:28 -0800

    Comments added

--- Find_nums_with_sum_as_element.py
+++ Find_nums_with_sum_as_element.py
@@ -1,8 +1,11 @@
+# This python program is used to list out index number of
+# numbers  which equate to a sum. The sum will be part of 
+# the list.
 import timeit
 list1 = [0,2,3,4,5,6,7,8,9]
 sum = int(input("Enter the sum to found:"))
 start = 0
-
+# We will not look for the indices until the end of ths list
 if(sum < int(max(list1))):
    end = int(max(list1)/2) + 1
 else:
