51800044c3d666130af5d44d8bd86a1263b3c02d
sudarsandm <sudarsandm@gmail.com>
2017-01-17 13:50:04 -0800

    ip address grep tool added

---
+++ ip_adress_grep.pl
@@ -0,0 +1,24 @@
+use strict;
+use warnings;
+use Carp;
+
+my $ip = <STDIN>;
+chomp($ip);
+
+if($ip =~ /(\d{1,3}\.)(\d{1,3}\.)(\d{1,3}\.)(\d{1,3})/){
+	print("Entered value is in ip-address format\n");
+	print("$1\t$2\t$3\t$4\n");
+}
+else {
+	print("Not an ip-address\n");
+}
+my @values = split(/\./,$ip);
+foreach (@values){
+	if (($_ >=0) && ($_ <=255)){
+		next;
+	}
+	else{
+		die("Not an Ip-address\n");
+	}
+}
+print("entered value is an ip-address\n")
