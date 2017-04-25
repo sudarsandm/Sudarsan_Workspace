be7778a9794564523b16bf42857f9d5e8a0ed13e
sudarsandm <sudarsandm@gmail.com>
2017-04-25 06:28:08 +0000

    more tests added for is_anagram.py

--- is_anagram.py
+++ is_anagram.py
@@ -16,3 +16,5 @@ def is_anagram(s1,s2):
 		
 print("Test code runs here...\n")
 print(is_anagram('S  i  Ri','I r    iS'))
+print(is_anagram('clint eastwood','old west action'))
+print(is_anagram('public relations','crap built on lies'))
