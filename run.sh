571e4be62a77140e9f39e873db4ef7d6431970e9
sudarsandm <sudarsandm@gmail.com>
2017-04-29 22:55:12 +0000

    updated run.sh with messages

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
+echo 'Testing Find_nums_with_sum_as_element'
 python3 Find_nums_with_sum_as_element.py 9 
+echo 'Testing is_anagram program'
 python3 is_anagram.py
+echo 'Testing is_prime program'
 python3 is_prime.py
+echo 'Testing anagram_revisited program'
 python3 anagram_revisited.py
