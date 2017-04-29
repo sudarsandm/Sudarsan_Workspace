29813f817ee101c9b28556cfe15c43ed4bb4c413
sudarsandm <sudarsandm@gmail.com>
2017-04-29 22:32:25 +0000

    revisited the anagram program

--- anagram_revisited.py
+++ anagram_revisited.py
@@ -16,21 +16,6 @@ def is_anagram(s1,s2):
             return False
     return True
 
-from nose.tools import assert_equal
-
-class AnagramTest(object):
-
-   def test(self,sol):
-      assert_equal(sol('go go go','gggOOO'),True)
-      assert_equal(sol('Abc','CBa'),True)
-      assert_equal(sol('Abc','CBa'),True)
-      assert_equal(sol('hi man','hi   man'),True)
-      assert_equal(sol('aabbcc','aabbc'),False)
-      assert_equal(sol('123','1 2'),False)
-      print("ALL TEST CASES PASSED")
-
 # Run Tests
-t = AnagramTest()
-t.test(anagram)
-
-    
+print(is_anagram('go go go','gggoOO'))
+print(is_anagram('121','122'))
