071ec028e539981d07e518231fdfe024a84be260
sudarsandm <sudarsandm@gmail.com>
2018-03-09 00:04:38 -0800

    Added Unit tests for MissingElement.py

--- MissingElement.py
+++ MissingElement.py
@@ -1,18 +1,16 @@
-# This program is for finding the missing elements between two arrays which has
-# all elements same except for one.
-
 def finder(arr1,arr2):
-   miss_item = 0
-   for item in arr1:
-      if (arr2.count(item) == 0):
-         miss_item = item
-      else:
-         continue
+    """
+    This function finds the missing element between two arrays having same 
+    values except for one
+    """
+    miss_item = 0
+    for item in arr1:
+        if (arr2.count(item) == 0):
+            miss_item = item
+        else:
+            continue
       
-   if (miss_item == 0):
-      return False
-   else:
-      return miss_item
-
-print(finder([1,4,3,2,5],[1,3,4,5]))
-print(finder([7,4,3,2,5],[7,3,2,5]))
+    if (miss_item == 0):
+        return None 
+    else:
+        return miss_item
---
+++ test_MissingElement.py
@@ -0,0 +1,15 @@
+import unittest
+from MissingElement import finder 
+
+class TestFinder(unittest.TestCase):
+
+    def test_finder(self):
+        """
+        This will test for missing element between two arrays.
+        """
+        self.assertEqual(finder([3,7,9,2], [2,7,9]), 3)
+        self.assertEqual(finder([3,7,9,2], [3,7,9]), 2)
+        self.assertEqual(finder([3,7,9,2], [2,7,3]), 3)
+
+if __name__ == '__main__':
+    unittest.main()
