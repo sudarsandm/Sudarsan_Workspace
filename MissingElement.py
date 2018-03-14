223a95df6bb3b0d1ed7a81c88ffe89b0c42eb5e1
sudarsandm <sudarsandm@gmail.com>
2018-03-14 13:26:56 -0700

    Updated MissingElement solution with dictionary based solution

--- MissingElement.py
+++ MissingElement.py
@@ -1,7 +1,9 @@
+from collections import defaultdict
 def finder(arr1,arr2):
     """
     This function finds the missing element between two arrays having same 
-    values except for one
+    values except for one. This approach uses count method associated with
+    lists data structure in python.
     """
     miss_item = 0
     for item in arr1:
@@ -14,3 +16,23 @@ def finder(arr1,arr2):
         return None 
     else:
         return miss_item
+def finder_dict(arr1, arr2):
+    """
+    This function finds the missing element using default dict from
+    collections module. We can also use an empty dictionary to use 
+    it for finding the missing element.
+    """
+    # create an empty dictionary which can hold integer values
+    numbers = defaultdict(int)
+
+    # Populate the dictionary with occurence of each element
+    for i in arr2:
+        numbers[i] += 1
+    # isolate the element by comparing the dictionary with arr1
+
+    for i in arr1:
+        if i in numbers:
+            numbers[i] -= 1
+        else:
+            return i
+    
--- test_MissingElement.py
+++ test_MissingElement.py
@@ -1,15 +1,23 @@
 import unittest
-from MissingElement import finder 
+from MissingElement import finder, finder_dict 
 
 class TestFinder(unittest.TestCase):
 
     def test_finder(self):
         """
-        This will test for missing element between two arrays.
+        This will test the array based approach. 
         """
         self.assertEqual(finder([3,7,9,2], [2,7,9]), 3)
         self.assertEqual(finder([3,7,9,2], [3,7,9]), 2)
         self.assertNotEqual(finder([3,7,9,2], [2,7,3]), 3)
+    def test_finder_dict(self):
+        """
+        This will test the dictionary based approach. 
+        """
+        self.assertEqual(finder_dict([3,7,9,2],[2,7,9]), 3)
+        self.assertEqual(finder_dict([3,7,9,2],[3,7,9]), 2)
+        self.assertNotEqual(finder_dict([3,7,9,2],[2,7,3]), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
