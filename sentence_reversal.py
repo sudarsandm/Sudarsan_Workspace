24430654b9c3d991d9d6774ffb8df330a7f9208a
sudarsandm <sudarsandm@gmail.com>
2018-03-17 12:44:56 -0700

    added another method for sentence reversal

--- sentence_reversal.py
+++ sentence_reversal.py
@@ -19,4 +19,38 @@ def reverse_using_temp(s):
 
     return " ".join(words)
 
+def reverse_using_whitespace(s):
+    """
+    We will look for word boundaries in terms of space in the sentence.
+    """
+    # Edge case check
+    if len(s) == 0:
+        return 0
 
+    words = []
+    length = len(s)
+    space = [' ']
+
+    i = 0
+
+    while i < length:
+        if s[i] not in space:
+            word_start = i
+            # increment i until we hit a space or word boundary
+            while i < length and s[i] not in space:
+                i += 1
+            # once the word boundary is reached append it words
+            words.append(s[word_start:i])
+        i += 1
+
+    j = 0
+    length = len(words)
+
+    # This code block is used to reverse the words
+    while j < int(length/2):
+        temp = words[j]
+        words[j] = words[length - 1 - j]
+        words[length - 1 - j] = temp
+        j += 1
+    
+    return " ".join(words)
--- test_sentence_reversal.py
+++ test_sentence_reversal.py
@@ -1,6 +1,11 @@
-from sentence_reversal import reverse_using_temp
+from sentence_reversal import reverse_using_temp, reverse_using_whitespace
 import pytest
 
 def test_reverse_using_temp():
     assert reverse_using_temp("I was in tpt.") == "tpt. in was I"
     assert reverse_using_temp("") == 0
+
+def test_reverse_using_whitespace():
+    assert reverse_using_whitespace("Twinkle Twinkle Little Star") == "Star" \
+            + " Little Twinkle Twinkle"
+    assert reverse_using_whitespace("") == 0
