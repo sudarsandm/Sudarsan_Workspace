00e737301ae3414e6f3edefd1e162a06e4d14104
sudarsandm <sudarsandm@gmail.com>
2018-03-18 00:46:54 -0700

    String compression solution added

---
+++ string_compression.py
@@ -0,0 +1,28 @@
+def compress(s):
+    """
+    Compress a string on the basis of number of characters being repeated.
+    For instance "AABBB" should compress to "A2B3".
+    """
+    # Edge case checking
+    if len(s) == 0:
+        return 0
+    
+    # Another Edge case check 
+    if len(s) == 1:
+        return s
+    
+    s = s.strip().rstrip()
+    d = {}
+
+    for i in s:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+    
+    ret = ""
+        
+    for i in sorted(d.keys()):
+        ret = ret + i + str(d[i])
+
+    return ret
---
+++ test_string_compression.py
@@ -0,0 +1,8 @@
+from string_compression import compress
+import pytest
+
+def test_compress():
+    assert compress("AAABBBCCCDDD") == "A3B3C3D3"
+    assert compress("AAB") == "A2B1"
+    assert compress("") == 0
+    assert compress('a') == 'a'
