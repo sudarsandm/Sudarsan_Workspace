6393ba2c6106028bde21e6f4314fe2a4fea76308
Sudarsan <sudarsandm@gmail.com>
2018-03-18 00:54:41 -0700

    Update test_string_compression.py
    updated the test result for single character string.

--- test_string_compression.py
+++ test_string_compression.py
@@ -5,4 +5,4 @@ def test_compress():
     assert compress("AAABBBCCCDDD") == "A3B3C3D3"
     assert compress("AAB") == "A2B1"
     assert compress("") == 0
-    assert compress('a') == 'a'
+    assert compress('a') == 'a1'
